47e6501acca8f5ae59fe8ee313b8409dbcdf2abe
LuizXIV <luizfelipemcg@gmail.com>
2025-12-14 12:00:39 -0300

    ...
    Refactor Categoria class to an interface with method definitions.

--- src/Entidades/Categoria.java
+++ src/Entidades/Categoria.java
@@ -1,24 +1,8 @@
 package entidades;
 
-public class Categoria {
+public interface Categoria {
 
-    private double tarifaBase, precoPorKm;
-
-
-    public Categoria(double tarifaBase, double precoPorKm) {
-        this.tarifaBase = tarifaBase;
-        this.precoPorKm = precoPorKm;
-    }
-
-
-    public double getTarifaBase() {return tarifaBase;}
-
-    public double getPrecoPorKm() {return precoPorKm;}
-
-
-
-    public static final Categoria comum = new Categoria(5.0, 1.0);
-    public static final Categoria luxo = new Categoria(9.0, 2.2);
-
+    double calcularPreco(double distanciaKm); 
+    String getNome();
 
 }
