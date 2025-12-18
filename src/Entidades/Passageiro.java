c08817a9c181132d2177528a904d476ce807daf7
LuizXIV <luizfelipemcg@gmail.com>
2025-12-18 18:59:04 -0300

    ...

--- src/Entidades/Passageiro.java
+++ src/Entidades/Passageiro.java
@@ -2,19 +2,28 @@ package entidades;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import excecoes.PassageiroPendenteException;
 import servicos.MetodoPagamento;
 
 public class Passageiro extends Usuario{
     private List<MetodoPagamento> metodosPagamento = new ArrayList<>();
 
+    public Passageiro() {}
+    
     public Passageiro(String nome, String cpf, String senha, String email, String telefone) {
         super(nome, cpf, senha, email, telefone);
     }
 
-    public Corrida solicitarCorrida(Local partida, Local destino, Categoria categoria) {
-        return new Corrida(partida, destino, categoria);
+    public Corrida solicitarCorrida(Passageiro passageiro, String partida, String destino,double distancia, Categoria categoria) {
+        return new Corrida(passageiro, partida, destino, distancia, categoria);
     }
 
+    public void checarPodeSolicitarCorrida() throws PassageiroPendenteException {
+        if (metodosPagamento.isEmpty()) {
+            throw new PassageiroPendenteException("Passageiro precisa adicionar um método de pagamento antes de solicitar uma corrida.");
+        }
+    }
 
     public void adicionarMetodoPagamento(MetodoPagamento metodo) {
         this.metodosPagamento.add(metodo);
@@ -28,5 +37,8 @@ public class Passageiro extends Usuario{
         return metodosPagamento.get(index);
     }
 
+    public List<MetodoPagamento> getMetodosPagamento() {
+        return metodosPagamento;
+    }
 
 }
