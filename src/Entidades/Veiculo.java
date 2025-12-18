4eaa43c727cac9e45b13017824e6b24e67864807
LuizXIV <luizfelipemcg@gmail.com>
2025-12-18 19:00:15 -0300

    ...

--- src/Entidades/Veiculo.java
+++ src/Entidades/Veiculo.java
@@ -7,6 +7,8 @@ public class Veiculo {
     private int ano;
     private Categoria categoria;
 
+    public Veiculo() {}
+    
     public Veiculo(String placa, String modelo, String cor, int ano, Categoria categoria) {
         this.placa = placa;
         this.modelo = modelo;
@@ -14,8 +16,18 @@ public class Veiculo {
         this.ano = ano;
         this.categoria = categoria;
     }
+
+    public String getCor() {return cor;}
+    public void setCor(String cor) {this.cor = cor;}
+    public int getAno() {return ano;}
+    public void setAno(int ano) {this.ano = ano;}
+    public Categoria getCategoria() {return categoria;}
+    public void setCategoria(Categoria categoria) {this.categoria = categoria;}
+    public String getPlaca() {return placa;}
+    public void setPlaca(String placa) {this.placa = placa;}
+    public String getModelo() {return modelo;}
+    public void setModelo(String modelo) {this.modelo = modelo;}
+
 }
-
-
 
 
