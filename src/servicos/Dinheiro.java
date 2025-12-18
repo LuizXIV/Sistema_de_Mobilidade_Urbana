5690dd6dfa55d2f06f189db26dcf1c74400836ae
LuizXIV <luizfelipemcg@gmail.com>
2025-12-18 19:01:45 -0300

    ...

--- src/servicos/Dinheiro.java
+++ src/servicos/Dinheiro.java
@@ -1,19 +1,22 @@
 package servicos;
 
+import entidades.Passageiro;
+import excecoes.PagamentoRecusadoException;
+import excecoes.SaldoInsuficienteException;
+
 public class Dinheiro implements MetodoPagamento{
-    private int valorRecebido;
+	@Override
+	public void processarPagamento(double valor, Passageiro passageiro) throws SaldoInsuficienteException, PagamentoRecusadoException {
+		try {
+			System.out.println("Pagamento em dinheiro de R$ " + valor + " processado com sucesso.");
+		} catch (Exception e) {
+			throw new PagamentoRecusadoException("Pagamento em dinheiro recusado.");
+		}
+	}
 
-    public Dinheiro(int valorRecebido) {
-        this.valorRecebido = valorRecebido;
-    }
-
-    @Override
-    public boolean processarPagamento(double valor) {
-        if (valorRecebido >= valor) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	@Override
+	public String getTipoPagamento() {
+		return "Dinheiro";
+	}
 
 }
