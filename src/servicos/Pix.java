a302cd1da138a43d59937173dbfed7af7d5c3100
LuizXIV <luizfelipemcg@gmail.com>
2025-12-18 19:02:17 -0300

    ...

--- src/servicos/Pix.java
+++ src/servicos/Pix.java
@@ -1,19 +1,31 @@
 package servicos;
 
+import entidades.Passageiro;
+import excecoes.PagamentoRecusadoException;
+import excecoes.SaldoInsuficienteException;
+
 public class Pix implements MetodoPagamento{
 
-    private String chavePix;
+	private String chavePix;
 
     public Pix(String chavePix) {
-        this.setChavePix(chavePix);
+        this.chavePix = chavePix;
     }
 
     @Override
-    public boolean processarPagamento(double valor) {
-        return true;
+    public void processarPagamento(double valor, Passageiro passageiro)
+            throws SaldoInsuficienteException, PagamentoRecusadoException {
+        if (chavePix == null || chavePix.isEmpty()) {
+            throw new PagamentoRecusadoException("Chave Pix inválida.");
+        }
+        System.out.println("Pagamento via Pix de R$ " + valor + " processado com sucesso");
     }
 
-    public String getChavePix() {return chavePix;}
-    public void setChavePix(String chavePix) {this.chavePix = chavePix;}
+    @Override
+    public String getTipoPagamento() {
+        return "Pix";
+    }
+
+    
 
 }
